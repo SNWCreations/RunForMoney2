eeae6ea35aecb778be21097f0ba50be1bb0793d8
ZX夏夜之风 <snwcreations@qq.com>
2023-01-09 14:56:42 +0800

    Cleanup invalid code

--- src/main/java/snw/rfm/Main.java
+++ src/main/java/snw/rfm/Main.java
@@ -2,29 +2,21 @@ package snw.rfm;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import snw.rfm.entity.GameManager;
 import snw.rfm.entity.TeamRegistry;
 
 public final class Main extends JavaPlugin {
     private static Main INSTANCE;
-    private GameManager gameManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         INSTANCE = this;
-        gameManager = new GameManager();
         TeamRegistry.init();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        gameManager.getGames().forEach(i -> i.stop());
-    }
-
-    public GameManager getGameManager() {
-        return gameManager;
     }
 
     public static Main getInstance() {
