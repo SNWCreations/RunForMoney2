5273ef01d67b454da641eb3430a3b576e5735e79
ZX夏夜之风 <snwcreations@qq.com>
2023-01-09 18:52:51 +0800

    Let the basic event work

--- src/main/java/snw/rfm/entity/Game.java
+++ src/main/java/snw/rfm/entity/Game.java
@@ -6,6 +6,9 @@ import org.apache.commons.lang.Validate;
 
 import org.bukkit.event.Listener;
 import snw.rfm.ConfigConstant;
+import snw.rfm.Main;
+import snw.rfm.events.GameStartEvent;
+import snw.rfm.events.GameStopEvent;
 import snw.rfm.listeners.AttackListener;
 import snw.rfm.tasks.CoinTimer;
 import snw.rfm.util.ListenerList;
@@ -27,12 +30,14 @@ public class Game {
         Validate.isTrue(!isRunning(), "The game is running.");
         running = true;
         // TODO game initialization
+        Main.getInstance().getServer().getPluginManager().callEvent(new GameStartEvent(this));
         listeners.add(new AttackListener(this));
         new CoinTimer(this, ConfigConstant.GAME_TIME * 60).start();
     }
 
     public void stop() {
         // TODO game termination
+        Main.getInstance().getServer().getPluginManager().callEvent(new GameStopEvent(this));
         listeners.clear();
     }
 
