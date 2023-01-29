a9a487e1e626de4124fea1408e6d69214c030ed3
ZX夏夜之风 <snwcreations@qq.com>
2023-01-29 17:50:27 +0800

    Add CoinMap#toView

--- src/main/java/snw/rfm/entity/CoinMap.java
+++ src/main/java/snw/rfm/entity/CoinMap.java
@@ -1,8 +1,12 @@
 package snw.rfm.entity;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import snw.rfm.ConfigConstant;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,5 +36,15 @@ public class CoinMap {
                 player.getBukkitPlayer().getUniqueId().toString(),
                 (k, v) -> v * ConfigConstant.COIN_DELETION_MULTIPLIER
         );
+    }
+
+    public Map<OfflinePlayer, Double> toView() {
+        Map<OfflinePlayer, Double> result = new HashMap<>();
+        for (Map.Entry<String, Double> e : map.entrySet()) {
+            OfflinePlayer key = Bukkit.getOfflinePlayer(UUID.fromString(e.getKey()));
+            Double value = e.getValue();
+            result.put(key, value);
+        }
+        return Collections.unmodifiableMap(result);
     }
 }
