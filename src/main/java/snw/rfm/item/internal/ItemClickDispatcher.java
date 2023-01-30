1b2a8e8afb4f761cf5cae6a9182bc23fe39cff2d
ZX夏夜之风 <snwcreations@qq.com>
2023-01-30 10:05:15 +0800

    Item only works for players

--- src/main/java/snw/rfm/item/internal/ItemClickDispatcher.java
+++ src/main/java/snw/rfm/item/internal/ItemClickDispatcher.java
@@ -7,6 +7,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import snw.rfm.Main;
 import snw.rfm.api.item.ItemRegistry;
+import snw.rfm.entity.IngamePlayer;
+import snw.rfm.entity.TeamRegistry;
 
 import java.util.Optional;
 
@@ -21,15 +23,17 @@ public class ItemClickDispatcher implements Listener {
     public void onClick(PlayerInteractEvent e) {
         if (main.isGamePresent()) {
             if (e.hasItem()) {
-                if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-                    Optional.ofNullable(ItemRegistry.getCallback(e.getItem())).ifPresent(i -> {
-                        boolean remove = i.onClick(e.getPlayer(), e.getItem());
-                        if (remove) {
-                            // NotNull at this time
-                            // noinspection DataFlowIssue
-                            e.getItem().setAmount(e.getItem().getAmount() - 1);
-                        }
-                    });
+                if (TeamRegistry.RUNNER.contains(IngamePlayer.getWrappedPlayer(e.getPlayer()))) {
+                    if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                        Optional.ofNullable(ItemRegistry.getCallback(e.getItem())).ifPresent(i -> {
+                            boolean remove = i.onClick(e.getPlayer(), e.getItem());
+                            if (remove) {
+                                // NotNull at this time
+                                // noinspection DataFlowIssue
+                                e.getItem().setAmount(e.getItem().getAmount() - 1);
+                            }
+                        });
+                    }
                 }
             }
         }
