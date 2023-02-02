347844133299b6acc76cbf3e1bdcd88cbc3fc91a
ZX夏夜之风 <snwcreations@qq.com>
2023-02-02 12:35:07 +0800

    Support batch operation in /rfmteam join now

--- src/main/java/snw/rfm/commands/RFMTeamCommand.java
+++ src/main/java/snw/rfm/commands/RFMTeamCommand.java
@@ -1,5 +1,6 @@
 package snw.rfm.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,6 +16,7 @@ import snw.rfm.entity.TeamRegistry;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static snw.rfm.util.Util.*;
@@ -38,14 +40,23 @@ public class RFMTeamCommand implements TabExecutor {
                     if (args.length == 1) {
                         sender.sendMessage(pluginMsg(ChatColor.RED + "参数不足。"));
                         return false;
-                    }
-                    String teamName = args[1];
-                    Team team = TeamRegistry.getTeamByName(teamName);
-                    if (isInvisibleTeamName(teamName) || team == null) { // you can't join invisible team
-                        sender.sendMessage(pluginMsg(ChatColor.RED + "队伍不存在。"));
                     } else {
-                        team.add(IngamePlayer.getWrappedPlayer(((Player) sender)));
-                        sendSuccess(sender);
+                        String teamName = args[1];
+                        Team team = TeamRegistry.getTeamByName(teamName);
+                        if (isInvisibleTeamName(teamName) || team == null) { // you can't join invisible team
+                            sender.sendMessage(pluginMsg(ChatColor.RED + "队伍不存在。"));
+                        } else {
+                            if (args.length > 2) {
+                                if (sender.isOp()) {
+                                    batch(sender, args, 1, team::add);
+                                } else {
+                                    sender.sendMessage(pluginMsg(ChatColor.RED + "无权操作。"));
+                                }
+                            } else {
+                                team.add((Player) sender);
+                                sendSuccess(sender);
+                            }
+                        }
                     }
                 }
                 break;
@@ -88,6 +99,19 @@ public class RFMTeamCommand implements TabExecutor {
                 }
         }
         return Collections.emptyList();
+    }
+
+    private static void batch(CommandSender sender, String[] args, int start, Consumer<Player> action) {
+        for (int i = start; i < args.length; i++) {
+            String name = args[start];
+            Player player = Bukkit.getPlayerExact(name);
+            if (player != null) {
+                action.accept(player);
+                sender.sendMessage(pluginMsg(ChatColor.GREEN + "成功处理了 " + name));
+            } else {
+                sender.sendMessage(pluginMsg(ChatColor.RED + "处理 " + name + " 时失败: 找不到玩家。"));
+            }
+        }
     }
 
 }
