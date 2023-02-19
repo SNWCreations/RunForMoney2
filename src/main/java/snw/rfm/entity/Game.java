ab9889eec0ca28d769f7a08172dc69849c407da2
ZX夏夜之风 <snwcreations@qq.com>
2023-02-19 11:45:13 +0000

    Hunter release listener should use LOWEST priority

--- src/main/java/snw/rfm/entity/Game.java
+++ src/main/java/snw/rfm/entity/Game.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffect;
@@ -123,7 +124,7 @@ public class Game {
     }
 
     class HunterReleaseListener implements Listener {
-        @EventHandler
+        @EventHandler(priority = EventPriority.LOWEST)
         public void on(HunterReleasedEvent e) {
             if (e.getGame() == Game.this) {
                 Game.this.coinTimer = new CoinTimer(main, Game.this, timeRemaining);
