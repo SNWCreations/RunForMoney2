f4297afecce448aece9a7873540a441b3438f0fa
ZX夏夜之风 <snwcreations@qq.com>
2023-06-22 17:03:45 +0800

    Fix issue: NPE if you call /rfmtimer when the game is not running

--- src/main/java/snw/rfm/commands/RFMTimerCommand.java
+++ src/main/java/snw/rfm/commands/RFMTimerCommand.java
@@ -30,6 +30,9 @@ public class RFMTimerCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (isPlayer(sender)) {
+            if (!main.isGamePresent()) {
+                sender.sendMessage(pluginMsg(ChatColor.RED + "游戏未运行。"));
+            }
             if (ConfigConstant.NO_TIMER) {
                 ((Player) sender).spigot()
                         .sendMessage(ChatMessageType.ACTION_BAR,
