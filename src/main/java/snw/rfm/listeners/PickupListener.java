a37197e178e5b8fb2ef495fb9ef3a09f81576dce
ZX夏夜之风 <snwcreations@qq.com>
2023-08-29 08:59:18 +0800

    Hunters can't pickup anything now

--- src/main/java/snw/rfm/listeners/PickupListener.java
+++ src/main/java/snw/rfm/listeners/PickupListener.java
@@ -5,9 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
-import snw.rfm.commands.SlowItemCommand;
 import snw.rfm.entity.TeamRegistry;
 
 public class PickupListener implements Listener {
@@ -15,15 +12,7 @@ public class PickupListener implements Listener {
     public void onPick(EntityPickupItemEvent e) {
         if (e.getEntity() instanceof Player) {
             if (TeamRegistry.HUNTER.contains(((Player) e.getEntity()))) {
-                ItemStack stack = e.getItem().getItemStack();
-                if (stack.hasItemMeta()) {
-                    if (stack.getItemMeta()
-                            .getPersistentDataContainer()
-                            .has(SlowItemCommand.SLOW_KEY, PersistentDataType.INTEGER)
-                    ) {
-                        e.setCancelled(true);
-                    }
-                }
+                e.setCancelled(true);
             }
         }
     }
